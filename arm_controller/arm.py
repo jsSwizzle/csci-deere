48c3c3dab0866d680a3de83fe9e1c93b88333bb6
jsSwizzle <jarod.smith@live.com>
2021-02-18 22:53:27 -0600

    initial setup of KDL chain and solvers

--- arm_controller/arm.py
+++ arm_controller/arm.py
@@ -18,38 +18,41 @@ class Arm(Robot):
     def __init__(self):
         """Constructs Arm class.
         """
-        servo_info = {}
-        servo_info['s1'] = {'function':'waist','default_value':90.0, 'min_value':0.0, 'max_value':180.0}
-        servo_info['s2'] = {'function':'shoulder','default_value':150.0, 'min_value':0.0, 'max_value':180.0}
-        servo_info['s3'] = {'function':'elbow','default_value':35.0, 'min_value':0.0, 'max_value':180.0}
-        servo_info['s4'] = {'function':'wrist_roll','default_value':140.0, 'min_value':0.0, 'max_value':180.0}
-        servo_info['s5'] = {'function':'wrist_pitch','default_value':85.0, 'min_value':0.0, 'max_value':180.0}
-        servo_info['s6'] = {'function':'grip','default_value':80.0, 'min_value':0.0, 'max_value':180.0}
-        self._servo_info = servo_info
+        # TODO: update dictionaries according to what is needed for solver
+        joint_info = {}
+        joint_info['jt1'] = {'function':'waist','default_value':90.0, 'min_value':0.0, 'max_value':180.0,}
+        joint_info['jt2'] = {'function':'shoulder','default_value':150.0, 'min_value':0.0, 'max_value':180.0}
+        joint_info['jt3'] = {'function':'elbow','default_value':35.0, 'min_value':0.0, 'max_value':180.0,}
+        joint_info['jt4'] = {'function':'wrist_roll','default_value':140.0, 'min_value':0.0, 'max_value':180.0,}
+        joint_info['jt5'] = {'function':'wrist_pitch','default_value':85.0, 'min_value':0.0, 'max_value':180.0}
+        joint_info['jt6'] = {'function':'grip','default_value':80.0, 'min_value':0.0, 'max_value':180.0}
+        # TODO: determine if claw grip should be in joint info or seperate as its not needed for solver
+        self._joint_info = joint_info
 
         segment_info = {}
-        segment_info['seg1'] = {'base_servo':'s1','segment_length':1.0,'axis_of_rotation':'Z'}
-        segment_info['seg2'] = {'base_servo':'s2','segment_length':120.0,'axis_of_rotation':'Y'}
-        segment_info['seg3'] = {'base_servo':'s3','segment_length':1.0,'axis_of_rotation':'Y'}
-        segment_info['seg4'] = {'base_servo':'s4','segment_length':1.0,'axis_of_rotation':'X'}
-        segment_info['seg5'] = {'base_servo':'s5','segment_length':1.0,'axis_of_rotation':'Y'}
-        self._segment_info = arm_info
+        segment_info['seg1'] = {'head_joint':'jt1','segment_length':10.0}
+        segment_info['seg2'] = {'head_joint':'jt2','segment_length':120.0}
+        segment_info['seg3'] = {'head_joint':'jt3','segment_length':1.0}
+        segment_info['seg4'] = {'head_joint':'jt4','segment_length':1.0}
+        segment_info['seg5'] = {'head_joint':'jt5','segment_length':1.0}
+        self._segment_info = segment_info
 
         current_angles = {}
-        current_angles['s1'] = 0.0
-        current_angles['s2'] = 0.0
-        current_angles['s3'] = 0.0
-        current_angles['s4'] = 0.0
-        current_angles['s5'] = 0.0
-        current_angles['s6'] = 0.0
+        current_angles['jt1'] = 0.0
+        current_angles['jt2'] = 0.0
+        current_angles['jt3'] = 0.0
+        current_angles['jt4'] = 0.0
+        current_angles['jt5'] = 0.0
+        current_angles['jt6'] = 0.0
+        # TODO: determine if claw grip (jt6) should be stored in current angles to make fk/ik solution returns neater
         self._current_angles = current_angles
 
         self._servo_speed = 1.0
-        self._solver = Solver(servo_info, segment_info)
+        self._solver = Solver(segment_info, joint_info)
         self._kit = ServoKit(channels=16)
         self.configure_board()
 
-    def move_to(self, x_pos, y_pos, z_pos, roll=0, pitch=0):
+    def move_to(self, x_pos, y_pos, z_pos, roll=0, pitch=0, yaw=0):
         """Moves the arm to the specified position.
 
         Calculates and moves the arm so the claw is centered at the
@@ -62,9 +65,9 @@ class Arm(Robot):
             roll {float} -- Final roll angle of the wrist (default to 0).
             pitch {float} -- Final pitch angle of the wrist (default to 0).
         """
-        angles = self._solver.specific_inverse_solve(x_pos, y_pos, z_pos, roll, pitch)
-        for angle in angles:
-            self.set_part(angle, angles[angle]['final_angle'])
+        angles = self._solver.specific_inverse_solve(self._current_angles, x_pos, y_pos, z_pos, roll, pitch, yaw)
+        for joint in angles:
+            self.set_part(joint, angles[joint]['final_angle'])
 
     def get_pos(self):
         """Calculates and returns current position of the arm.
@@ -75,7 +78,8 @@ class Arm(Robot):
         Return:
             A list containing the (x, y, z) position of the claw.
         """
-        self._solver.forward_solve()
+        current_pos = self._solver.forward_solve(self._current_angles)
+        return current_pos
 
     def set_speed(self, ss):
         """Set's the speed at which the servo's move.
@@ -91,9 +95,9 @@ class Arm(Robot):
     def configure_board(self):
         """Sets mapping for Servo ID to Servo Number.
         """
-        servo_no = 0
-        for servo in self._servo_info:
-            self._servo_info[servo]['servo#'] = servo_no
+        joint_no = 0
+        for joint in self._joint_info:
+            self._joint_info[joint]['joint#'] = servo_no
             servo_no += 1
 
     def set_default_position(self):
@@ -102,8 +106,8 @@ class Arm(Robot):
         Sets each servo to its default position found in the servo_info dictionary
         created during class initialization.
         """
-        for servo, info in self._servo_info.items():
-            self.set_part(servo, info['default_value'])
+        for joint, info in self._joint_info.items():
+            self.set_part(joint, info['default_value'])
 
     def set_part(self, part, value):
         """Moves the specified part.
@@ -114,5 +118,6 @@ class Arm(Robot):
             part {str} -- item part to move
             value {float} -- value to apply to part
         """
-        self._kit.servo[self._servo_info[part]['servo#']].angle = value
+        # TODO: figure out how the speed/rate can be implemented in setting a part to its value
+        self._kit.servo[self._joint_info[part]['servo#']].angle = value
         self._current_angles[part] = value
