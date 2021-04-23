c2ca6ac05b231ea66152b45d7bdd5ee88d1146f6
Jarod Smith <jarod.smith@live.com>
2021-04-22 23:33:49 -0500

    fixed install required >=

--- setup.py
+++ setup.py
@@ -23,12 +23,12 @@ setuptools.setup(
         "arm_controller.test"
     ],
     install_requires=[
-        "numpy=>1.20.1",
-        "scipy=>1.6.1",
+        "numpy>=1.20.1",
+        "scipy>=1.6.1",
         "adafruit-circuitpython-servokit",
-        "matplotlib=>3.3.4",
-        "ikpy=>3.1",
-        "roboticstoolbox-python=>0.9.1"
+        "matplotlib>=3.3.4",
+        "ikpy>=3.1",
+        "roboticstoolbox-python>=0.9.1"
     ],
     python_requires=">=3.6"
 )
